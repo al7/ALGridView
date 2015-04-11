2f469bd8ceab67fa0a98f62c5d960ef302b9e88e
Alex Leite <admin@al7dev.com>
2015-04-11 11:59:37 -0700

    Grid View Implementation

--- ALGridView/Source/GridView.swift
+++ ALGridView/Source/GridView.swift
@@ -1,13 +1,82 @@
-//
-//  GridView.swift
-//  ALGridView
-//
-//  Created by Alexandre Leite on 4/11/15.
-//  Copyright (c) 2015 Alexandre Leite. All rights reserved.
-//
+/*
+
+Copyright (c) 2015 - Alex Leite (al7dev)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 
 import UIKit
 
-class GridView: NSObject {
-   
+public typealias GridViewConstructor = (column: UInt, row: UInt, contentView: UIView) -> Void
+
+public class GridView: UIView {
+    
+    private var _padding: CGFloat = 0.0
+    private var _margins: UIEdgeInsets = UIEdgeInsetsZero
+    private var _columns: UInt = 0
+    private var _rows: UInt = 0
+    
+    public var padding: CGFloat { return self._padding }
+    public var margins: UIEdgeInsets { return self._margins }
+    public var columns: UInt { return self._columns }
+    public var rows: UInt { return self._rows }
+    
+    //MARK- Initializers
+    
+    public init(frame: CGRect, columns: UInt, rows: UInt, margins: UIEdgeInsets, padding: CGFloat, constructor: GridViewConstructor!) {
+        super.init(frame: frame)
+        
+        self._columns = (columns >= 1) ? columns : 1
+        self._rows = (rows >= 1) ? rows : 1
+        self._margins = margins
+        self._padding = padding
+        
+        var contentFrame = self.bounds
+        contentFrame.origin.x = self.margins.left
+        contentFrame.origin.y = self.margins.top
+        contentFrame.size.width -= (self.margins.left + self.margins.right)
+        contentFrame.size.height -= (self.margins.top + self.margins.bottom)
+        
+        var cellSize = contentFrame.size
+        cellSize.width = (cellSize.width - (padding * CGFloat(columns - 1))) / CGFloat(columns)
+        cellSize.height = (cellSize.height - (padding * CGFloat(rows - 1))) / CGFloat(rows)
+        
+        for r in 0..<self.rows {
+            for c in 0..<self.columns {
+                var contentFrame = CGRect.zeroRect
+                contentFrame.origin.x = self.margins.left + (CGFloat(c) * padding) + (CGFloat(c) * cellSize.width)
+                contentFrame.origin.y = self.margins.top + (CGFloat(r) * padding) + (CGFloat(r) * cellSize.height)
+                contentFrame.size = cellSize
+                
+                let contentView = UIView(frame: contentFrame)
+                constructor(column: c, row: r, contentView: contentView)
+                self.addSubview(contentView)
+            }
+        }
+    }
+    
+    convenience init(frame: CGRect, columns: UInt, rows: UInt, constructor: GridViewConstructor!) {
+        self.init(frame: frame, columns: columns, rows: rows, margins: UIEdgeInsetsZero, padding: 0.0, constructor: constructor)
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- ALGridView/ViewController.swift
+++ ALGridView/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
     //MARK- View Lifecycle
 
     override func loadView() {
-        self.view = UIView(frame: UIScreen.mainScreen().bounds)
-        self.view.backgroundColor = UIColor.blueColor()
+        self.view = ViewControllerView()
     }
     
     override func viewDidLoad() {
@@ -22,5 +21,47 @@ class ViewController: UIViewController {
 
     }
 
+}
+
+class ViewControllerView: UIView {
+    
+    var gridView: GridView?
+    
+    //MARK- Initializers
+    
+    init() {
+        super.init(frame: UIScreen.mainScreen().bounds)
+        self.backgroundColor = UIColor.whiteColor()
+        
+        var gridFrame = CGRect.zeroRect
+        gridFrame.size.width = min(self.bounds.size.width, self.bounds.size.height)
+        gridFrame.size.height = gridFrame.size.width
+        
+        var margins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        /** SAMPLE CODE BELOW **/
+        
+        self.gridView = GridView(frame: gridFrame, columns: 3, rows: 3, margins: margins, padding: 10.0) {
+            column, row, contentView in
+            
+            let label = UILabel(frame: contentView.bounds)
+            label.textAlignment = .Center
+            label.backgroundColor = UIColor.lightGrayColor()
+            label.textColor = UIColor.darkGrayColor()
+            label.text = "\(column)x\(row)"
+            contentView.addSubview(label)
+        }
+        self.addSubview(self.gridView!)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK- Base Overrides
+    
+    override func layoutSubviews() {
+        self.gridView?.center = self.center
+    }
 }
 
